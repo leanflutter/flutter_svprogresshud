3695357267daed8e5e63d7c51e9bb52e9e0e7988
JianyingLi <lijy91@foxmail.com>
2019-11-07 23:07:45 +0800

    移除 macos 平台支持相关文件

--- example/macos/Flutter/GeneratedPluginRegistrant.swift
+++ example/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,7 +5,7 @@
 import FlutterMacOS
 import Foundation
 
-import flutter_svprogresshud
+import flutter_svprogresshud_fde
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FlutterSvprogresshudPlugin.register(with: registry.registrar(forPlugin: "FlutterSvprogresshudPlugin"))
