84febd2fe8725f08f68b59ee41fcec3a895fed7d
LiJianying <lijy91@foxmail.com>
2020-10-27 19:46:45 +0800

    Update example

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,11 +1,6 @@
-import 'package:flutter/foundation.dart'
-    show debugDefaultTargetPlatformOverride;
-import 'dart:async';
-import 'dart:io';
-
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
-import 'package:flutter_svprogresshud/flutter_svprogresshud.dart';
+
+import 'pages/home.dart';
 
 void main() {
   runApp(MyApp());
@@ -18,75 +13,9 @@ class MyApp extends StatefulWidget {
 
 class _MyAppState extends State<MyApp> {
   @override
-  void initState() {
-    SVProgressHUD.dismiss();
-    SVProgressHUD.setDefaultStyle("light");
-    SVProgressHUD.setDefaultMaskType("none");
-    super.initState();
-  }
-
-  @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Plugin example app'),
-        ),
-        body: Column(
-          children: <Widget>[
-            ListTile(
-              title: Text('show'),
-              onTap: () {
-                SVProgressHUD.show("Loading...");
-                SVProgressHUD.dismissWithDelay(1500);
-              },
-            ),
-            ListTile(
-              title: Text('showSuccess'),
-              onTap: () {
-                SVProgressHUD.showSuccess("Success");
-                SVProgressHUD.dismissWithDelay(1500);
-              },
-            ),
-            ListTile(
-              title: Text('showInfo'),
-              onTap: () {
-                SVProgressHUD.showInfo("Info");
-                SVProgressHUD.dismissWithDelay(1500);
-              },
-            ),
-            ListTile(
-              title: Text('showError'),
-              onTap: () {
-                SVProgressHUD.showError("Error");
-                SVProgressHUD.dismissWithDelay(1500);
-              },
-            ),
-            ListTile(
-              title: Text('showProgress'),
-              onTap: () {
-                int progress = 1;
-                Timer.periodic(const Duration(milliseconds: 100), (timer) {
-                  print(progress);
-                  progress += 1;
-                  if (progress > 100) {
-                    timer.cancel();
-                    SVProgressHUD.dismiss();
-                    return;
-                  }
-                  SVProgressHUD.showProgress((progress), "Loading...");
-                });
-              },
-            ),
-            ListTile(
-              title: Text('dismiss'),
-              onTap: () {
-                SVProgressHUD.dismiss();
-              },
-            )
-          ],
-        ),
-      ),
+      home: HomePage(),
     );
   }
 }
